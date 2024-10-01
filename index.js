8db1052acffa69549e993b99e973023204b793e9
fay-ui <faith.njau@student.moringaschool.com>
2024-10-01 09:07:49 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun('go to club'));
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
